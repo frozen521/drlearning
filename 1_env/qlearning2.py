77a51ed1d62c81b0d538c1deeb152ed78a40b2cf
copyer1998 <2782644531@qq.com>
2019-04-19 15:21:26 +0800

    :construction_worker: Adding CI build system.

--- 1_env/qlearning2.py
+++ 1_env/qlearning2.py
@@ -20,7 +20,6 @@ valid_actions = np.array([[1, 3, 4],
                           [1, 2, 4],
                           [0, 3, 4],
                           [0, 2, 4]])
-
 for i in range(1000):
     start_state = 0
     current_state = start_state
@@ -30,7 +29,10 @@ for i in range(1000):
         future_rewards = []
         for action_nxt in valid_actions[next_state]:
             future_rewards.append(q_matrix[next_state][action_nxt])
+            print(future_rewards)
+            print('这是分割线')
         q_state = reward[current_state][action] + gamma * max(future_rewards)
+        print(q_state)
         q_matrix[current_state][action] = q_state
         # print(q_matrix)
         current_state = next_state
